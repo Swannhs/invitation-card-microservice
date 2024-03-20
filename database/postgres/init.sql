3e2556e10692cb3ce54a53c3b38c77b4f2719230
Swannhs <princeapurbask@gmail.ocm>
2024-03-20 15:27:32 -0400

    Initialize django event management project

--- database/postgres/init.sql
+++ database/postgres/init.sql
@@ -1,6 +1,17 @@
 CREATE DATABASE "event-api";
+CREATE DATABASE "card-api";
+CREATE DATABASE "event-management-api";
+
 
 GRANT ALL PRIVILEGES ON DATABASE "event-api" TO root;
+GRANT ALL PRIVILEGES ON DATABASE "card-api" TO root;
+GRANT ALL PRIVILEGES ON DATABASE "event-management-api" TO root;
 
 \c "event-api"
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+\c "card-api"
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+\c "event-management-api"
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
